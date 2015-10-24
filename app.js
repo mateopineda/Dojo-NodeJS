650ffc01e9cd58affb5892c5bc07266ca121e33c
mateopineda <you@example.com>
2015-10-24 10:34:22 -0500

    stotify2

---
+++ app.js
@@ -0,0 +1,38 @@
+var express=require('express');
+var app=express();
+var hbs=require('hbs');
+var https=require('https');
+
+/* Ste Templare engine */
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+/* Routes */
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+app.get('/search', function(req,res){
+    var endPointSpotify = "https://api.spotify.com/v1/search?q="+req.query.q+"&type=track&limit=10";
+    var buffer="";
+    https.get(endPointSpotify,function(response){
+    	response.on('data',function(d){
+    		buffer+=d;
+    	});
+
+    	response.on('end',function(err){
+    		if(req.query.q!=undefined && req.query.q!=""){
+    			res.render('index',{ items: JSON.parse(buffer).tracks.items});
+    		}else{
+    			res.render('index');
+    		}
+    	});
+    });
+});
+
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log('Node Server Running');
+}); //puerto 3000
